0716b55426f94d5cd7638465b6f95a9b81f24643
imarov11 <saidibakaskambarbekovich@gmail.com>
2022-03-13 21:19:17 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 23 , i = 45, b = 38;
+
+        System.out.println( " summasy: " +( 23 + 45 + 38) );
+        System.out.println("koboitundusu: " + (23 * 45 * 38));
+        System.out.println("arif orto san:"+ (23+45+ 38)/ (double)3);
+    }
+}
